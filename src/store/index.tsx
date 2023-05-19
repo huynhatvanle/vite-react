5e13880cc68a17dc1bfbcc89d2a4d420379cdafd
ducnguyen <ducnn.230288@gmail.com>
2023-05-19 15:02:24 +0700

    fix(image): add lazyload

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -4,21 +4,9 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { Action } from './action';
 import { Slice, State } from './slice';
 import { globalSlice, GlobalFacade, User } from './global';
-import { userSlice, UserFacade } from './user';
-import { userRoleSlice, UserRoleFacade, UserRole } from './user/role';
-import { codeSlice, CodeFacade } from './code';
-import { codeTypeSlice, CodeTypeFacade } from './code/type';
-import { dataSlice, DataFacade } from './data';
-import { dataTypeSlice, DataTypeFacade } from './data/type';
 
 const rootReducer = combineReducers({
   [globalSlice.name]: globalSlice.reducer,
-  [userSlice.name]: userSlice.reducer,
-  [userRoleSlice.name]: userRoleSlice.reducer,
-  [codeSlice.name]: codeSlice.reducer,
-  [codeTypeSlice.name]: codeTypeSlice.reducer,
-  [dataSlice.name]: dataSlice.reducer,
-  [dataTypeSlice.name]: dataTypeSlice.reducer,
 });
 
 const setupStore = () => {
@@ -30,20 +18,5 @@ const setupStore = () => {
 const useAppDispatch = () => useDispatch<ReturnType<typeof setupStore>['dispatch']>();
 const useTypedSelector: TypedUseSelectorHook<ReturnType<typeof rootReducer>> = useSelector;
 
-export {
-  Action,
-  Slice,
-  User,
-  UserRole,
-  setupStore,
-  useAppDispatch,
-  useTypedSelector,
-  GlobalFacade,
-  UserFacade,
-  UserRoleFacade,
-  CodeFacade,
-  CodeTypeFacade,
-  DataFacade,
-  DataTypeFacade,
-};
+export { Action, Slice, User, setupStore, useAppDispatch, useTypedSelector, GlobalFacade };
 export type { State };
