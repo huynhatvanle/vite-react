e50d42f173db5d28006ebe02b24c2a377ee71b8f
ducnnn <ducnn.230288@gmail.com>
2023-05-22 19:46:59 +0700

    fix(cypress): autotest login

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -15,6 +15,7 @@ const Page = () => {
   }, []);
 
   const [swiperRef, setSwiperRef] = useState<SwiperClass>();
+  const [swiperRef1, setSwiperRef1] = useState<SwiperClass>();
   gsap.registerPlugin(SplitText);
   const animationSlide = (swiper: SwiperClass, delay: number) => {
     const tl = gsap.timeline({
@@ -79,13 +80,12 @@ const Page = () => {
           onInit={(e) => animationSlide(e, 0)}
           onSlideChangeTransitionStart={(e) => animationSlide(e, 0.5)}
         >
-          <SwiperSlide className={'flex items-center h-96'}>
+          {[1,2,3,4].map((i) => <SwiperSlide key={i} className={'flex items-center h-96'}>
             <div className={'container mx-auto px-12'}>
               <h1 className="split-text text-2xl xl:text-4xl text-blue-500 font-bold leading-none mt-8">
-                {' '}
-                Enhance Vietnam’s intellectual value{' '}
+                Enhance Vietnam’s intellectual value
               </h1>
-              <p className="text-gray-500 leading-none top"> Choose the service you want us to help with </p>
+              <p className="text-gray-500 leading-none top">{i} Choose the service you want us to help with </p>
               <div className="right mt-10 mb-3 ng-star-inserted">
                 <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">Digital Transformation </p>
                 <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">
@@ -107,16 +107,38 @@ const Page = () => {
             </div>
             <img
               className={'lazy zoom w-full h-96 object-cover absolute top-0 left-0 -z-10'}
-              data-src="https://via.placeholder.com/440x560?text=Img+01"
+              data-src={'https://via.placeholder.com/440x560?text=Img+0' + i}
             />
-          </SwiperSlide>
-          <SwiperSlide className={'flex items-center h-96'}>
-            <div className={'container mx-auto px-12'}>
-              <h1 className="split-text text-2xl xl:text-4xl text-blue-500 font-bold leading-none mt-8">
-                {' '}
-                Enhance Vietnam’s intellectual value{' '}
+          </SwiperSlide>)}
+        </Swiper>
+      </div>
+      <div className={'relative group bg-gray-100'}>
+        <div className="absolute z-10 w-full h-0 top-[calc(50%-1.125rem)]">
+          <div className={'flex  justify-between container mx-auto'}>
+            <Arrow
+              className="w-9 h-9 rotate-180 transition-all duration-500 ease-in-out relative -left-20 group-hover:-left-12 opacity-0 group-hover:opacity-100 bg-gray-300 hover:bg-blue-600 hover:text-white p-2 rounded-full cursor-pointer"
+              onClick={() => swiperRef1?.slidePrev()}
+            />
+            <Arrow
+              className="w-9 h-9 transition-all duration-500 ease-in-out relative -right-20 group-hover:-right-12 opacity-0 group-hover:opacity-100 bg-gray-300 hover:bg-blue-600 hover:text-white p-2 rounded-full cursor-pointer"
+              onClick={() => swiperRef1?.slideNext()}
+            />
+          </div>
+        </div>
+        <Swiper
+          onSwiper={setSwiperRef1}
+          loop={true}
+          slidesPerView={3}
+          spaceBetween={16}
+          centeredSlides={true}
+          className={'container slide-zoom'}
+        >
+          {[1,2,3,4,5,6,7,8,9].map((i) => <SwiperSlide key={i} className={'flex items-center h-96'}>
+            <div className={'container mx-auto'}>
+              <h1 className="split-text text-xl xl:text-2xl text-blue-500 font-bold leading-none mt-8">
+                Enhance Vietnam’s intellectual value
               </h1>
-              <p className="text-gray-500 leading-none top"> Choose the service you want us to help with </p>
+              <p className="text-gray-500 leading-none top">{i} Choose the service you want us to help with </p>
               <div className="right mt-10 mb-3 ng-star-inserted">
                 <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">Digital Transformation </p>
                 <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">
@@ -130,82 +152,13 @@ const Page = () => {
                 </p>
               </div>
               <a
-                onClick={() => console.log('2')}
+                onClick={() => console.log('1')}
                 className="bottom bg-blue-500 text-white px-5 py-3 inline-block rounded-3xl"
               >
                 Get Started <i className="las la-arrow-right ml-1"></i>
               </a>
             </div>
-            <img
-              className={'lazy zoom w-full h-96 object-cover absolute top-0 left-0 -z-10'}
-              alt="Row 01, col 02"
-              data-src="https://via.placeholder.com/440x560?text=Img+02"
-            />
-          </SwiperSlide>
-          <SwiperSlide className={'flex items-center h-96'}>
-            <div className={'container mx-auto px-12'}>
-              <h1 className="split-text text-2xl xl:text-4xl text-blue-500 font-bold leading-none mt-8">
-                {' '}
-                Enhance Vietnam’s intellectual value{' '}
-              </h1>
-              <p className="text-gray-500 leading-none top"> Choose the service you want us to help with </p>
-              <div className="right mt-10 mb-3 ng-star-inserted">
-                <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">Digital Transformation </p>
-                <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">
-                  <i className="las la-check-circle"></i> R&amp;D Services
-                </p>
-                <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">
-                  <i className="las la-check-circle"></i> Outsourcing Services
-                </p>
-                <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">
-                  <i className="las la-check-circle"></i> Product Development
-                </p>
-              </div>
-              <a
-                onClick={() => console.log('3')}
-                className="bottom bg-blue-500 text-white px-5 py-3 inline-block rounded-3xl"
-              >
-                Get Started <i className="las la-arrow-right ml-1"></i>
-              </a>
-            </div>
-            <img
-              className={'lazy zoom w-full h-96 object-cover absolute top-0 left-0 -z-10'}
-              alt="Row 01, col 01"
-              data-src="https://via.placeholder.com/440x560?text=Img+03"
-            />
-          </SwiperSlide>
-          <SwiperSlide className={'flex items-center px-12 h-96'}>
-            <div className={'container mx-auto px-12'}>
-              <h1 className="split-text text-2xl xl:text-4xl text-blue-500 font-bold leading-none mt-8">
-                {' '}
-                Enhance Vietnam’s intellectual value{' '}
-              </h1>
-              <p className="text-gray-500 leading-none top"> Choose the service you want us to help with </p>
-              <div className="right mt-10 mb-3 ng-star-inserted">
-                <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">Digital Transformation </p>
-                <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">
-                  <i className="las la-check-circle"></i> R&amp;D Services
-                </p>
-                <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">
-                  <i className="las la-check-circle"></i> Outsourcing Services
-                </p>
-                <p className="font-bold text-2xl xl:text-3xl text-blue-800 ng-star-inserted">
-                  <i className="las la-check-circle"></i> Product Development
-                </p>
-              </div>
-              <a
-                onClick={() => console.log('4')}
-                className="bottom bg-blue-500 text-white px-5 py-3 inline-block rounded-3xl"
-              >
-                Get Started <i className="las la-arrow-right ml-1"></i>
-              </a>
-            </div>
-            <img
-              className={'lazy zoom w-full h-96 object-cover absolute top-0 left-0 -z-10'}
-              alt="Row 01, col 02"
-              data-src="via.placeholder.com/440x560?text=Img+04"
-            />
-          </SwiperSlide>
+          </SwiperSlide>)}
         </Swiper>
       </div>
     </Fragment>
