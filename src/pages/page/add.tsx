40718470bdbaf810013f7490114851a4c247efdc
nguyenluongnhan <110026254+nguyenluongnhan@users.noreply.github.com>
2023-06-05 16:16:35 +0700

    Update add.tsx

--- src/pages/page/add.tsx
+++ src/pages/page/add.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from 'react-router';
 import slug from 'slug';
