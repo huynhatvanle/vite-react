f7403ccc243c289a52a3a008018401e1a8559544
ducnguyen <ducnn.230288@gmail.com>
2023-06-07 13:46:43 +0700

    fix(image): add lazyload

--- src/pages/code/add.tsx
+++ src/pages/code/add.tsx
@@ -56,7 +56,7 @@ const Page = () => {
   const listType = (result?.data || []).map((item) => ({ value: item.code, label: item.name }));
   const { t } = useTranslation();
   return (
-    <div className={'max-w-4xl mx-auto'}>
+    <div className={'max-w-2xl mx-auto'}>
       <Form
         values={{ ...codeFacade.data }}
         className="intro-x"
