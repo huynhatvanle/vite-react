ba752e232e157d4ec56e6398f5de692c544b3d98
ducnnn <ducnn.230288@gmail.com>
2023-06-08 21:14:22 +0700

    fix(cypress): autotest login

--- src/pages/code/index.tsx
+++ src/pages/code/index.tsx
@@ -9,6 +9,7 @@ import { Edit, Plus, Trash } from '@svgs';
 import { TableRefObject } from '@models';
 import { Popconfirm, Spin, Tooltip } from 'antd';
 import { useNavigate } from 'react-router';
+import classNames from "classnames";
 
 const Page = () => {
   const { user, setBreadcrumbs } = GlobalFacade();
@@ -33,15 +34,17 @@ const Page = () => {
     }
   }, [codeFacade.status]);
 
-  const dataTableRef = useRef<TableRefObject>(null);
+  const request = JSON.parse(codeFacade.queryParams || '{}');
+  request.filter = JSON.parse(request?.filter || '{}');
   const { t } = useTranslation();
+  const dataTableRef = useRef<TableRefObject>(null);
   const navigate = useNavigate();
   return (
     <div className={'container mx-auto grid grid-cols-12 gap-3 px-2.5 pt-2.5'}>
       <div className="col-span-12 md:col-span-4 lg:col-span-3 -intro-x">
         <div className="shadow rounded-xl w-full bg-white overflow-hidden">
           <div className="h-14 flex justify-between items-center border-b border-gray-100 px-4 py-2">
-            <h3 className={'font-bold text-xl'}>Type Code</h3>
+            <h3 className={'font-bold text-lg'}>Type Code</h3>
             {/*<div className="flex items-center">*/}
             {/*  <Button*/}
             {/*    icon={<Plus className="icon-cud !h-5 !w-5" />}*/}
@@ -53,47 +56,52 @@ const Page = () => {
           <Spin spinning={isLoading}>
             <div className="h-[calc(100vh-13rem)] overflow-y-auto relative scroll">
               {result?.data?.map((data, index) => (
-                <button
+                <div
                   key={data.id}
-                  className={
-                    'item text-gray-700 font-medium hover:bg-gray-100 flex justify-between items-center border-b border-gray-100 w-full text-left px-4 h-10 group'
-                  }
+                  className={classNames({'bg-gray-100': request.filter.type === data.code},'item text-gray-700 font-medium hover:bg-gray-100 flex justify-between items-center border-b border-gray-100 w-full text-left  group')}
                 >
-                  <span className="truncate cursor-pointer flex-1 hover:text-blue-500 item-text">
+                  <div
+                    onClick={() => {
+                      if (request.filter.type !== data.code) request.filter.type = data.code;
+                      else delete request.filter.type;
+                      dataTableRef?.current?.onChange(request)
+                    }}
+                    className="truncate cursor-pointer flex-1 hover:text-blue-500 item-text px-4 py-2"
+                  >
                     {index + 1}. {data.name}
-                  </span>
-                  <span className="w-16 flex justify-end gap-1">
-                    {user?.role?.permissions?.includes(keyRole.P_CODE_TYPE_UPDATE) && (
-                      <Tooltip title={t('routes.admin.Layout.Edit')}>
-                        <button
-                          className={'opacity-0 group-hover:opacity-100 transition-all duration-300 '}
-                          title={t('routes.admin.Layout.Edit') || ''}
-                          onClick={() => navigate(`/${lang}${routerLinks('Code')}/${data.id}/edit`)}
-                        >
-                          <Edit className="icon-cud bg-blue-600 hover:bg-blue-400" />
-                        </button>
-                      </Tooltip>
-                    )}
-                    {user?.role?.permissions?.includes(keyRole.P_CODE_TYPE_DELETE) && (
-                      <Tooltip title={t('routes.admin.Layout.Delete')}>
-                        <Popconfirm
-                          placement="left"
-                          title={t('components.datatable.areYouSureWant')}
-                          onConfirm={() => dataTableRef?.current?.handleDelete!(data.id || '')}
-                          okText={t('components.datatable.ok')}
-                          cancelText={t('components.datatable.cancel')}
-                        >
-                          <button
-                            className={'opacity-0 group-hover:opacity-100 transition-all duration-300'}
-                            title={t('routes.admin.Layout.Delete') || ''}
-                          >
-                            <Trash className="icon-cud bg-red-600 hover:bg-red-400" />
-                          </button>
-                        </Popconfirm>
-                      </Tooltip>
-                    )}
-                  </span>
-                </button>
+                  </div>
+                  {/*<span className="w-16 flex justify-end gap-1">*/}
+                  {/*  {user?.role?.permissions?.includes(keyRole.P_CODE_TYPE_UPDATE) && (*/}
+                  {/*    <Tooltip title={t('routes.admin.Layout.Edit')}>*/}
+                  {/*      <button*/}
+                  {/*        className={'opacity-0 group-hover:opacity-100 transition-all duration-300 '}*/}
+                  {/*        title={t('routes.admin.Layout.Edit') || ''}*/}
+                  {/*        onClick={() => navigate(`/${lang}${routerLinks('Code')}/${data.id}/edit`)}*/}
+                  {/*      >*/}
+                  {/*        <Edit className="icon-cud bg-blue-600 hover:bg-blue-400" />*/}
+                  {/*      </button>*/}
+                  {/*    </Tooltip>*/}
+                  {/*  )}*/}
+                  {/*  {user?.role?.permissions?.includes(keyRole.P_CODE_TYPE_DELETE) && (*/}
+                  {/*    <Tooltip title={t('routes.admin.Layout.Delete')}>*/}
+                  {/*      <Popconfirm*/}
+                  {/*        placement="left"*/}
+                  {/*        title={t('components.datatable.areYouSureWant')}*/}
+                  {/*        onConfirm={() => dataTableRef?.current?.handleDelete!(data.id || '')}*/}
+                  {/*        okText={t('components.datatable.ok')}*/}
+                  {/*        cancelText={t('components.datatable.cancel')}*/}
+                  {/*      >*/}
+                  {/*        <button*/}
+                  {/*          className={'opacity-0 group-hover:opacity-100 transition-all duration-300'}*/}
+                  {/*          title={t('routes.admin.Layout.Delete') || ''}*/}
+                  {/*        >*/}
+                  {/*          <Trash className="icon-cud bg-red-600 hover:bg-red-400" />*/}
+                  {/*        </button>*/}
+                  {/*      </Popconfirm>*/}
+                  {/*    </Tooltip>*/}
+                  {/*  )}*/}
+                  {/*</span>*/}
+                </div>
               ))}
             </div>
           </Spin>
@@ -135,10 +143,6 @@ const Page = () => {
                   title: 'Code.Type',
                   name: 'type',
                   tableItem: {
-                    filter: {
-                      type: 'radio',
-                      list: listType || [],
-                    },
                     width: 110,
                     sorter: true,
                     render: (text: string) => text && listType.filter((item) => item.value === text)[0]?.label,
