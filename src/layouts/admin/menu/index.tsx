4c33582aa66b77f96f577575a47bc43eab481aab
nguyenluongnhan <110026254+nguyenluongnhan@users.noreply.github.com>
2023-07-24 10:02:28 +0700

    menu Merchandise-managerment

--- src/layouts/admin/menu/index.tsx
+++ src/layouts/admin/menu/index.tsx
@@ -40,16 +40,18 @@ const Layout = ({ isCollapsed = false, permission = [] }: { isCollapsed: boolean
       {child
         .filter((subItem: any) => !subItem.permission || permission?.includes(subItem.permission))
         .map((subItem: any, index: number) => (
-          <a
-            key={index + v4()}
-            className={classNames('child-item py-2 cursor-pointer rounded-2xl text-gray-300 font-medium text-base', {
-              'bg-teal-700 text-white !fill-gray-300':
-                location.pathname.indexOf(`/${lang}${routerLinks(subItem.name)}`) > -1,
-            })}
-            onClick={() => navigate(`/${lang}${routerLinks(subItem.name)}`)}
-          >
-            <span>{t(`titles.${subItem.name}`)}</span>
-          </a>
+          <li className={classNames('flex items-center pl-9 py-2 group cursor-pointer rounded-2xl text-gray-300 font-medium text-base', {
+            'bg-teal-700 text-white !fill-gray-300':
+              location.pathname.indexOf(`/${lang}${routerLinks(subItem.name)}`) > -1,
+          })}>
+            <p className='h-1 w-1 mr-3 rounded-lg bg-white group-hover:w-2 duration-300 ease-in-out transition-all'></p>
+            <a
+              key={index + v4()}
+              onClick={() => navigate(`/${lang}${routerLinks(subItem.name)}`)}
+            >
+              <span>{t(`titles.${subItem.name}`)}</span>
+            </a>
+          </li>
         ))}
     </ul>
   );
